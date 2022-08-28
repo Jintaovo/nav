d7d4666fd22a84f5f3390e1f98b690258ddd918e
Jintaovo <169359169@qq.com>
2022-08-28 12:18:16 +0800

    update

--- nav0更新.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-C:
-cd C:\Github\nav
-git add .&&git commit -m "update"&&git push origin master
---
+++ nav更新.bat
@@ -0,0 +1,4 @@
+@echo off
+C:
+cd C:\Github\nav
+git add .&&git commit -m "update"&&git push origin master
